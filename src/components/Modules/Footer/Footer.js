69990991136babdcb146f69360aa3c9a01f11e3e
amir4976 <amirhosain4976@gmail.com>
2025-03-26 17:50:10 +0330

    #

--- src/components/Modules/Footer/Footer.js
+++ src/components/Modules/Footer/Footer.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Footer() {
   return (
     <div className='w-full h-fit  flex flex-col justify-center items-center p-5 text-gray-500 text-xs'>
-      <p>©طراحی و نمونه از فرهاد اقدسی</p>
       <p>©دولوپ و برنامه نویسی توسط امیرحسین احمدی</p>
     </div>
   )
